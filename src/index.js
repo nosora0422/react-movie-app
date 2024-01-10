d1044f7c5c5782b4bc88567239e874c523a35c9c
Sora Noh <snoh7@dtc01l0860-12.ad.bcit.ca>
2024-01-09 18:36:23 -0800

    json updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { HashRouter, Routes, Route } from "react-router-dom"; 
 import './index.css';
 import App from './App';
 import NowPlaying from './views/NowPlaying/NowPlaying';
@@ -13,7 +13,7 @@ import Favorite from './views/Favorite/Favorite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<NowPlaying />} />
@@ -25,6 +25,6 @@ root.render(
         <Route path="favorite" element={<Favorite/>} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
